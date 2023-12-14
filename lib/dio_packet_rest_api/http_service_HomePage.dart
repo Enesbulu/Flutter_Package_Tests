9876f23966962203005916bba1ddf5564a8aa61c
Enes BULUT <74255515+Enesbulu@users.noreply.github.com>
2023-12-14 11:07:01 +0300

    gizli bilgiler kaldırıldı.

--- lib/dio_packet_rest_api/http_service_HomePage.dart
+++ lib/dio_packet_rest_api/http_service_HomePage.dart
@@ -9,7 +9,7 @@
 //     const String baseUrl = "https://jsonplaceholder.typicode.com/posts";
 //     var result = await http.get("https://jsonplaceholder.typicode.com/posts" as Uri);
 //     // var result = await http.get(baseUrl);
-//     // const String baseUrl = 'https://api.isarapp.com/api';
+//    
 
 //     const String customPoi = "$baseUrl/v1/custompois";
 
@@ -52,51 +52,3 @@
 
 
 // /*
-
-// static final String baseUrl = 'https://api.isarapp.com/api';
-
-// static final String customPoi = "$baseUrl/v1/custompois";
-
-// Future<void> getCustomPoiList(String parameters) async {
-// try {
-// _getToken();
-// isLoading.value = true;
-//   items = RxList<CustomPoiDto>();
-//   final response = await http.get(
-//       UrlHelper.customPoi + "?pageSize=1000&" + parameters,
-//       headers: {'Authorization': 'Bearer ' + _authToken});
-
-//   final extractedData = json.decode(response.body) as List<dynamic>;
-
-//   extractedData.forEach((element) {
-//     items.add(CustomPoiDto.fromJson(element));
-//   });
-
-//   isLoading.value = false;
-// } catch (e) {
-//   throw e;
-// }
-
-// ‌class CustomPoiDto {
-//   String id;
-//   String name;
-//   String description;
-//   CustomPoiTypeDto customPoiType;
-//   List<DetailStructureDto> details;
-//   List<String> documentPaths;
-//   AddressModel addressDetail;
-
-//   CustomPoiDto({
-//     this.id,
-//     this.name,
-//     this.description,
-//     this.customPoiType,
-//     this.details,
-//     this.documentPaths,
-//     this.addressDetail,
-//   });
-
-//   factory CustomPoiDto.fromJson(Map<String, dynamic> json) =>
-//       _$CustomPoiDtoFromJson(json);
-//   Map<String, dynamic> toJson() => _$CustomPoiDtoToJson(this);
-// */
