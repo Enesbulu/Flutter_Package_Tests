c3a9cf4566a4f2b717401ce8e9b20e23ba7132f4
Enes BULUT <74255515+Enesbulu@users.noreply.github.com>
2024-01-02 12:15:47 +0300

    token ve api bilgileri gitignor doyasına eklneid

--- lib/maps_with_api/main_maps_with_api.dart
+++ lib/maps_with_api/main_maps_with_api.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:mobil_test_projesi1/maps_with_api/isar_app_token.dart';
 import 'package:mobil_test_projesi1/maps_with_api/models/custom_pois_model.dart';
 import 'package:http/http.dart' as http;
 
@@ -70,19 +71,7 @@ class MyWidget extends StatelessWidget {
           return Text('Hata: ${snapshot.error}');
         } else {
           // Future başarıyla tamamlandıysa
-          return Scaffold(
-            appBar: AppBar(
-              title: const Text('Material App Bar'),
-            ),
-            body: GoogleMap(
-              mapType: MapType.hybrid,
-              initialCameraPosition: const CameraPosition(
-                target: LatLng(41.015137, 28.979530),
-                zoom: 15,
-              ),
-              markers: <Marker>{}.toSet(), // temp null değilse markers'a ekleyin
-            ),
-          );
+          return const MapsWithApi();
         }
       },
     );
@@ -114,16 +103,12 @@ class AddressDetailMarker extends IMarkerModel {
 }
 
 class RequestService extends State<GoogleMap> {
-  static const String _baseUrl = "https://api.isarapp.com/api";
   late CustomPoisModel custompoiModel;
-  static const String _token =
-      "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI0MWRiMjQ5Yy05NDJmLTRmNmQtYWY3ZC04MDUwOTU3NDc3ZWQiLCJmb3VuZGF0aW9uIjoiYzk3OGY5YTctN2U1YS00ZDBlLWFjODQtMTI1ZTgzMjY1YWMyIiwibGFuZyI6InRyIiwibmJmIjoxNzAyOTgzMTM1LCJleHAiOjE3MDUwNTY3MzUsImlhdCI6MTcwMjk4MzEzNX0.Q4BhAPnbza1NfFJxGIIqmpcIpHZoYTgwkvYlIHOlcEpVsCj_3Ee2Mn2dBuY8U-84kznMuLaM0ntvKe0FmzubIw";
 
-  static const Map<String, String> _header = <String, String>{'Authorization': 'Bearer $_token', 'Content-Type': 'application/json'};
+  static const Map<String, String> _header = <String, String>{'Authorization': 'Bearer ${TokenInfo.token}', 'Content-Type': 'application/json'};
 
   Future<List<AddressDetailMarker>?> getCustomPois() async {
-    const String _CustomPoiEndPoint = "/v1/custompois";
-    final Uri _urlValue = Uri.parse(_baseUrl + _CustomPoiEndPoint);
+    final Uri _urlValue = Uri.parse(TokenInfo.baseUrl + TokenInfo.CustomPoiEndPoint);
     final _customPois = await http.get(_urlValue, headers: _header);
     AddressDetailMarker addressDetailsMarker = AddressDetailMarker();
     List<AddressDetailMarker> listAddressdetailMarkers = [];
@@ -135,16 +120,8 @@ class RequestService extends State<GoogleMap> {
       print(customPoisModel);
 
       for (var i = 0; i < customPoisModel.length; i++) {
-        // print(customPoisModel);
-        // print('İd: ${customPoisModel[i].addressDetail?.latitude}  '
-        //     'Name: ${customPoisModel[i].addressDetail?.longitude} '
-        //     'description: ${customPoisModel[i].addressDetail?.longitude} '
-        //     'documentPaths: ${customPoisModel[i].addressDetail?.note}  '
-        //     'parentId: ${customPoisModel[i].addressDetail?.iconType} '
-        //     'isDeleted: ${customPoisModel[i].addressDetail?.iconColor}');
         addressDetailsMarker.latitude = customPoisModel[i].addressDetail!.latitude!;
         addressDetailsMarker.longitude = customPoisModel[i].addressDetail!.longitude!;
-
         listAddressdetailMarkers.add(addressDetailsMarker);
       }
       for (var element in listAddressdetailMarkers) {
@@ -175,9 +152,3 @@ class MyHttpOverrides extends HttpOverrides {
     return super.createHttpClient(context)..badCertificateCallback = (X509Certificate cert, String host, int port) => true;
   }
 }
-
-// Future<String> fetchData() async {
-//   // Asenkron olarak veri almak için kullanılan bir Future örneği
-//   await Future.delayed(const Duration(seconds: 2)); // Simüle edilen bir gecikme
-//   return 'Örnek Veri';
-// }
